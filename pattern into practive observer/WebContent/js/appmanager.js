fababa81cfa0690fa4473d21a3298ac5f448ae81
Nalaka <Nalaka@Dul-PC>
2015-12-04 01:32:16 +0530

    initial checkin pattern in practice observer

---
+++ pattern into practive observer/WebContent/js/appmanager.js
@@ -0,0 +1,29 @@
+﻿require([ "observer", "observers", "subject"], function ( observer, observers, subject) {
+
+
+            var subject, observer, otherObserver, data, moreData,
+                Subject = require('subject'),
+                Observer = require('observer');
+
+            subject = new Subject();
+            observer = new Observer('observer1');
+            otherObserver = new Observer('observer2');
+
+            data = {
+                prop: 'something'
+            };
+            moreData = {
+                prop: 'something else'
+            };
+
+            subject.observe(observer);
+            subject.observe(otherObserver);
+
+            subject.add(data);
+            subject.add(moreData);
+
+            subject.unObserve(observer);
+
+            subject.remove(data);
+
+});
